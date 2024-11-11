5029a077d292bef5f9d64e2ca2efa5d50fddc517
Marcus Nölke <marnoelke@googlemail.com>
2024-11-11 06:02:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    var member = this;
+    member.skills = [
+        {name: "Javascript", level: 5},
+        {name: "HTML", level: 5},
+        {name: "CSS", level: 5}
+    ];
+}
+
